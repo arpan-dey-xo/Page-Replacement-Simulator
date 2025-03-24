902b8e66c8636f48700bd86156e08b24e87f9495
Arpan Dey <midecl.arpan@gmail.com>
2025-03-24 16:27:34 +0530

    modified controller.py to plot graph
    made some slight changes to be able to plot a graph to show number of page faults in case of each algorithm.

--- controller.py
+++ controller.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from algorithms import simulate_fifo, simulate_lru, simulate_optimal
 
 def run_simulation_detailed(pages_str, capacity, algorithm):
@@ -20,6 +21,33 @@ def run_simulation_detailed(pages_str, capacity, algorithm):
         return "Unknown algorithm. Please select FIFO, LRU, or Optimal.", None
     return steps, faults
 
+def plot_graph(results):
+    algorithms = list(results.keys())
+    page_faults = list(results.values())
+
+    plt.bar(algorithms, page_faults, color=["skyblue", "lightgreen", "salmon"])
+    plt.xlabel("Page Replacement Algorithms")
+    plt.ylabel("Number of Page Faults")
+    plt.title("Comparison of Page Faults")
+    plt.tight_layout()
+    plt.show()
+
 def run_simulation(pages_str, capacity, algorithm):
-    steps, faults = run_simulation_detailed(pages_str, capacity, algorithm)
-    return faults
+    try:
+        pages = [int(x.strip()) for x in pages_str.split(",") if x.strip() != ""]
+    except Exception as e:
+        return f"Error parsing pages: {e}"
+
+    results = {}
+    steps_fifo, faults_fifo = simulate_fifo(pages, capacity)
+    results['FIFO'] = faults_fifo
+
+    steps_lru, faults_lru = simulate_lru(pages, capacity)
+    results['LRU'] = faults_lru
+
+    steps_opt, faults_opt = simulate_optimal(pages, capacity)
+    results['Optimal'] = faults_opt
+
+    plot_graph(results)
+
+    return results[algorithm.upper()]
